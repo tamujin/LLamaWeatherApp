d6b1b288617f01929cc45c82c6b556985b911ae9
tamujin <w@8work.de>
2025-01-19 21:53:54 +0100

    ollama test

--- LlamaWeatherApp/Program.cs
+++ LlamaWeatherApp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OllamaSharp;
+
+var uri = new Uri("http://localhost:11434");
+var ollama = new OllamaApiClient(uri);
+
+ollama.SelectedModel = "llama3";
+
+await foreach (var stream in ollama.GenerateAsync("generate c# factorial function in c#"))
+	Console.Write(stream.Response);
+
+Console.ReadLine();
