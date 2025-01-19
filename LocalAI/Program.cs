e04575cf5324258d1af94aa9ffaf26fc471e90ea
tamujin <w@8work.de>
2025-01-20 00:01:01 +0100

    solution file added

--- LocalAI/Program.cs
+++ LocalAI/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.AI;
+
+IChatClient chatClient =
+	new OllamaChatClient(new Uri("http://localhost:11434/"), "tinyllama");
+
+List<ChatMessage> chatHistory = new();
+
+while (true)
+{
+	Console.WriteLine("Your prompt:");
+	var userPrompt = Console.ReadLine();
+	chatHistory.Add(new ChatMessage(ChatRole.User, userPrompt));
+
+	Console.WriteLine("AI Response:");
+	var response = "";
+	await foreach (var item in
+	               chatClient.CompleteStreamingAsync(chatHistory))
+	{
+		Console.Write(item.Text);
+		response += item.Text;
+	}
+	chatHistory.Add(new ChatMessage(ChatRole.Assistant, response));
+	Console.WriteLine();
+}
